6bc2c0d2e9d0715ec8db1ef88b2bb96d97da16b9
hakyeop <127179902+hakyeop@users.noreply.github.com>
2024-03-25 17:30:17 +0900

    3주차 과제 하노이 타워
    하노이 타워

---
+++ hanoi_tower.cpp
@@ -0,0 +1,24 @@
+﻿#define CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void hanoi(int, char, char, char);
+
+void hanoi(int i, char start, char work, char target)
+{
+	if (i == 1)
+	{
+		printf("%c에서 원반 1를(을) %c로 옮김\n", start, target);
+	}
+	else
+	{
+		hanoi(i - 1, start, target, work);
+		printf("%c에서 원반 %d를(을) %c로 옮김\n", start, i, target);
+		hanoi(i - 1, work, start, target);
+	}
+}
+
+int main()
+{
+	hanoi(3, 'A', 'B', 'C');
+}
+
