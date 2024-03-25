f2de169edf120cfd8247434d88312a5ce15e457c
hakyeop <127179902+hakyeop@users.noreply.github.com>
2024-03-25 17:29:35 +0900

    2주차 과제2

---
+++ 2w_2.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	char s1[50];
+	int cnt = 0;
+
+	printf("문자열을 입력하세요 : ");
+	scanf("%s", &s1);
+
+	printf("\n입력된 문자열은\n");
+	printf("\"%s\"\n", s1);
+	printf("입니다.\n");
+
+	while (1)
+	{
+		if (s1[cnt] == '\0')
+			break;
+		else
+			cnt++;
+	}
+	printf("\n입력된 문자열의 길이 = %d\n", cnt);
+}
